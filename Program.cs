5f1d442758f88ca30cb78ed2d697af4d25917770
Ken Price <kenaprice@gmail.com>
2016-08-04 16:20:34 -0700

    Move test data, dictionary for test data, and methods to serialize data

--- Program.cs
+++ Program.cs
@@ -1,23 +1,71 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace aspnetcoreapp
 {
     public class Program
     {
+        public static Dictionary<int, Employee> testEmployees {get;  set;}
+        public static Dictionary<int, Department> testDepartments {get; set;}
+        public static string getSerializedEmployee(int id) {
+            XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+
+            using(StringWriter writer = new StringWriter()) {
+                Employee employee;
+                if (testEmployees.TryGetValue(id, out employee)) {
+                    serializer.Serialize(writer, testEmployees[id]);
+                    return writer.ToString();
+                } else {
+                    return null;
+                }
+            }
+        }
+        public static string getSerializedDepartment(int id) {
+            XmlSerializer serializer = new XmlSerializer(typeof(Department));
+
+            using(StringWriter writer = new StringWriter()) {
+                Department dept;
+                if (testDepartments.TryGetValue(id, out dept)) {
+                    serializer.Serialize(writer, testDepartments[id]);
+                    return writer.ToString();
+                } else {
+                    return null;
+                }
+            }
+        }
+        private static void populateTestData() {
+            // Please excuse the test data.
+            var employees = new List<Employee>();
+            var departments = new List<Department>();
+            employees.Add(new Employee(1, "Price", "Ken"));
+            employees.Add(new Employee(2, "Price", "Jason"));
+            employees.Add(new Employee(3, "Teskey", "Samantha"));
+            employees.Add(new Employee(4, "Thomas", "Justin"));
+            employees.Add(new Employee(5, "Average", "Joe"));
+            employees.Add(new Employee(6, "Tim", "Chad"));
+            departments.Add(new Department(1, "IT and Fire Safety"));
+            departments.Add(new Department(2, "Marketing"));
+            departments[0].addEmployee(employees[0]);
+            departments[0].addEmployee(employees[2]);
+            departments[1].addEmployee(employees[1]);
+            departments[1].addEmployee(employees[3]);
+            departments[1].addEmployee(employees[4]);
+            departments[1].addEmployee(employees[5]);
+            testEmployees = new Dictionary<int, Employee>();
+            testDepartments = new Dictionary<int, Department>();
+            employees.ForEach(delegate(Employee emp) {
+                testEmployees.Add(emp.id, emp);
+            });
+            departments.ForEach(delegate(Department dep) {
+                testDepartments.Add(dep.id, dep);
+            });
+        }
         public static void Main(string[] args)
         {
-            var testEmployees = new List<Employee>();
-            testEmployees.Add(new Employee(1, "Price", "Ken"));
-            testEmployees.Add(new Employee(2, "Price", "Jason"));
-            testEmployees.Add(new Employee(3, "Teskey", "Samantha"));
-            testEmployees.Add(new Employee(4, "Thomas", "Justin"));
-            testEmployees.Add(new Employee(5, "Average", "Joe"));
-            testEmployees.Add(new Employee(6, "Tim", "Chad"));
-            var itDeparment = new Department(1, "IT and Fire Safety");
-
-            var host = new WebHostBuilder()
+            populateTestData();
+            IWebHost host = new WebHostBuilder()
                 .UseKestrel()
                 .UseStartup<Startup>()
                 .Build();
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ namespace aspnetcoreapp
     {
         public void Configure(IApplicationBuilder app)
         {
+            int idParam;
+            string output = null;
             app.Run(context =>
             {
                 Console.Out.WriteLine(context.Request.Path);
@@ -18,11 +20,25 @@ namespace aspnetcoreapp
                 }
                 switch (context.Request.Path) {
                     case "/employees":
-                        Console.Out.WriteLine(context.Request.Query["id"]);
-                        break;
+                        if (!int.TryParse(context.Request.Query["id"], out idParam)) {
+                            return context.Response.WriteAsync("Please provide a valid id for employee!");
+                        }
+                        output = Program.getSerializedEmployee(idParam);
+                        if (output == null) {
+                            return context.Response.WriteAsync("Not found");
+                        } else {
+                            return context.Response.WriteAsync(output);
+                        }
                     case "/departments":
-                        Console.Out.WriteLine(context.Request.Query["id"]);
-                        break;
+                        if (!int.TryParse(context.Request.Query["id"], out idParam)) {
+                            return context.Response.WriteAsync("Please provide a valid id for department!");
+                        }
+                        output = Program.getSerializedDepartment(idParam);
+                        if (output == null) {
+                            return context.Response.WriteAsync("Not found");
+                        } else {
+                            return context.Response.WriteAsync(output);
+                        }
                 }
                 return context.Response.WriteAsync("Hello from ASP.NET Core!");
             });
