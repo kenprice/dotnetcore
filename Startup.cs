0f8a7f1ba4c0edec8ceae41a6eedbd3e8ee6cc61
Ken Price <kenaprice@gmail.com>
2016-08-04 15:45:19 -0700

    Add something like routes

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace aspnetcoreapp
 {
@@ -11,6 +12,18 @@ namespace aspnetcoreapp
         {
             app.Run(context =>
             {
+                Console.Out.WriteLine(context.Request.Path);
+                if (!context.Request.Path.HasValue) {
+                    return context.Response.WriteAsync("Hello there! You can request an employee by navigating to /employees?id=1 or a department with /departments?id=1");
+                }
+                switch (context.Request.Path) {
+                    case "/employees":
+                        Console.Out.WriteLine(context.Request.Query["id"]);
+                        break;
+                    case "/departments":
+                        Console.Out.WriteLine(context.Request.Query["id"]);
+                        break;
+                }
                 return context.Response.WriteAsync("Hello from ASP.NET Core!");
             });
         }
